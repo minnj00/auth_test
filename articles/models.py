9c4cd6fcb141c9001572791cbca20010f3df2e27
KangMinju <demo1208@gmail.com>
2023-08-29 16:27:07 +0900

    article_create

--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
 class Article(models.Model):
-    title=models.CharField(max_length=50)
-    content=models.TextField()
-    
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
